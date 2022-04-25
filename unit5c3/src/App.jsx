aed9e0cb0a47633e22343c67c988fc6eaea7f4db
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-04-25 16:23:09 +0530

    commit

---
+++ unit5c3/src/App.jsx
@@ -0,0 +1,28 @@
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { EmployeeList } from "./components/EmployeeList";
+import { EmployeeDetails } from "./components/EmployeeDetails";
+import { Admin } from "./components/Admin";
+// import { ProtectedRoute } from "./components/PrivateRoute";
+import { Navbar } from "./components/Navbar";
+import { Logout } from "./components/Logout";
+import {Routes,Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+      <Routes>{/* Routes here */}
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/employees" element={<EmployeeList></EmployeeList>}></Route>
+      <Route path="/admin" element={<Admin></Admin>}></Route>
+      <Route path="/logout" element={<Logout></Logout>}></Route>
+      <Route path="/Login" element={<Login></Login>}></Route>
+      
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
